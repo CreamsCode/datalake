be9a22a7a84b36b999e4f9aa430bce7af410556d
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-16 16:48:42 +0000

    Now S3 is created in console.

--- backend-setup.tf
+++ backend-setup.tf
@@ -1,9 +1,11 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "mi-terraform-backend-${random_string.suffix.result}"
+variable "bucket_name" {
+  default     = "mi-terraform-backend-${random_string.suffix.result}"
+  description = "Nombre del bucket S3"
+}
 
-  tags = {
-    Name = "Terraform State Bucket"
-  }
+variable "region" {
+  default     = "us-east-1"
+  description = "Región de AWS"
 }
 
 resource "random_string" "suffix" {
@@ -12,28 +14,28 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
+resource "null_resource" "create_bucket_and_upload" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if aws s3api head-bucket --bucket ${var.bucket_name} 2>/dev/null; then
+        echo "El bucket ${var.bucket_name} ya existe. No se necesita crear nuevamente."
+      else
+        echo "Creando el bucket ${var.bucket_name}..."
+        aws s3api create-bucket --bucket ${var.bucket_name} --region ${var.region} --create-bucket-configuration LocationConstraint=${var.region}
+        aws s3api put-bucket-versioning --bucket ${var.bucket_name} --versioning-configuration Status=Enabled
+      fi
+    EOT
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-  acl    = "public-read"
-}
-
 output "backend_bucket_name" {
-  value = aws_s3_bucket.terraform_state_bucket.bucket
+  value = var.bucket_name
 }
 
 output "backend_region" {
-  value = "us-east-1" # Región fija en este ejemplo
+  value = var.region
 }
 
 output "backend_key" {
   value = "terraform/state/terraform.tfstate"
 }
-
